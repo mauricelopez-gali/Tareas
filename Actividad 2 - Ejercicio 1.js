dc891361564f119f5d017790ae7a4928349bacbc
mauricelopez-gali <78775920+mauricelopez-gali@users.noreply.github.com>
2021-02-08 22:21:24 -0600

    Unidad 2
    Ejercicio de la unidad 2

---
+++ Actividad 2 - Ejercicio 1.js
@@ -0,0 +1,43 @@
+var mes = 10;
+switch (mes) {
+  case 12:
+    alert("es invierno");
+    break;
+  case 1:
+    alert("es invierno");
+    break;
+  case 2:
+    alert("es invierno");
+    break;
+  case 3:
+    alert("es primavera");
+    break;
+  case 4:
+    alert("es primavera");
+    break;
+  case 5:
+    alert("es primavera");
+    break;
+  case 6:
+    alert("es verano");
+    break;
+  case 7:
+    alert("es verano");
+    break;
+  case 8:
+    alert("es verano");
+    break;
+  case 9:
+    alert("es otoño");
+    break;
+  case 10:
+    alert("es otoño");
+    break;
+  case 11:
+    alert("es otoño");
+    break;
+  default:
+    alert("no es una opcion valida");
+    break;
+}
+alert("21004573 Maurice Alfonso Mauricio Lopez")
